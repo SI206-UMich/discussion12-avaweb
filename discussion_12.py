9c80db8f8e66fe1cbd335c4608f25a99a788a807
avaweb <avaweb@umich.edu>
2022-11-30 23:27:40 -0500

    done

--- discussion_12.py
+++ discussion_12.py
@@ -16,7 +16,8 @@ def setUpDatabase(db_name):
 # TASK 1
 # CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
 def create_employee_table(cur, conn):
-    pass
+    cur.execute("CREATE TABLE IF NOT EXIST Employee (employee_id INT, first_name TEXT, hire_date TEXT, job_id INT, salary INT")
+    conn.commit()
 
 # ADD EMPLOYEE'S INFORMTION TO THE TABLE
 
@@ -27,20 +28,39 @@ def add_employee(filename, cur, conn):
     file_data = f.read()
     f.close()
     # THE REST IS UP TO YOU
-    pass
+    employee_data = json.loads(file_data)
+    for item in employee_data:
+        employee_id = int(item['employee_id'])
+        first_name = item['first_name']
+        last_name = item['last_name']
+        hire_date = item['hire_date']
+        job_id = int(item['job_id'])
+        salary = int(item['salary'])
+        cur.execute('INSERT INTO Employees (employee_id, first_name, last_name, hire_date, job_id, salary) VALUES (?,?,?,?,?,?)',
+        (employee_id, first_name, last_name, hire_date, job_id, salary))
 
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
 def job_and_hire_date(cur, conn):
-    pass
+    cur.execute('SELECT Employees.hire _date, Jobs.job_title FROM Employees JOIN Jobs ON Employees.job_id, = Jobs.job_id')
+    job_hire_date = cur.fetchall()
+    conn.commit()
+
+    sorted_job_hire_date = sorted(job_and_hire_date, key = lambda x: x[0])
+    return sorted_job_hire_date
+
 
 # TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
 # Apply JOIN clause to match individual employees
 def problematic_salary(cur, conn):
-    pass
+    cur.execute('SELECT Employees.first_name, Employees.last_name FROM Employees JOIN Jobs ON Employees.job_id = Jobs.job_id')
+    conn.commit()
 
 # TASK 4: VISUALIZATION
 def visualization_salary_data(cur, conn):
-    pass
+    cur.execute('SELECT Employees.salary, Jobs.job_title FROM Employees join Jobs on Employees.job_id = Jobs.job_id')
+    salary_data = cur.fetchall()
+    conn.commit()
+    print(salary_data)
 
 class TestDiscussion12(unittest.TestCase):
     def setUp(self) -> None:
